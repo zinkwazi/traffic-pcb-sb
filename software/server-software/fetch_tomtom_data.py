afcac9c6dce175e540a28c4590d885a73f50b8b0
Jaden Baptista <baptistajaden@berkeley.edu>
2024-12-31 11:43:01 -0800

    add option to generate typical speeds file

--- software/server-software/fetch_tomtom_data.py
+++ software/server-software/fetch_tomtom_data.py
@@ -19,11 +19,18 @@ OUTPUT_NORTH = "data_north_V1_0_0.json"
 OUTPUT_SOUTH = "data_south_V1_0_0.json"
 OUTPUT_NORTH_2 = "data_north_V1_0_3.dat"
 OUTPUT_SOUTH_2 = "data_north_V1_0_3.dat"
+OUTPUT_NORTH_TYPICAL = "typical_north_V1_0_3.dat"
+OUTPUT_SOUTH_TYPICAL = "typical_south_V1_0_3.dat"
 LOG_FILE = "fetch_tomtom_data.log"
 
 class Direction(Enum):
     NORTH = 1
     SOUTH = 2
+    UNKNOWN = 3
+
+class SpeedType(Enum):
+    CURRENT = 1
+    TYPICAL = 2
     UNKNOWN = 3
 
 # ================================
@@ -71,7 +78,7 @@ def validEntry(entry):
 
     return entry["Latitude"] != None and entry["Longitude"] != None
 
-def requestData(entry):
+def requestData(entry, speed_type):
     '''Requests speed data from the TomTom API. Fails 
     if entry is invalid, which is checked with validEntry.
     Returns found speed if successful, -1 otherwise.'''
@@ -91,11 +98,18 @@ def requestData(entry):
         return -1
 
     json_response = response.json()
-    current_speed = int(json_response.get("flowSegmentData", {}).get("currentSpeed", -1))
-    if current_speed == -1:
+    if speed_type == SpeedType.CURRENT:
+        speed = int(json_response.get("flowSegmentData", {}).get("currentSpeed", -1))
+    elif speed_type == SpeedType.TYPICAL:
+        speed = int(json_response.get("flowSegmentData", {}).get("freeFlowSpeed", -1))
+    else:
+        log(f"Requested data for unknown speed type")
+        speed = -1
+
+    if speed == -1:
         log(f"No speed data found in response: {json_response}")
         return -1
-    return current_speed
+    return speed
 
 # ================================
 # General Functions
@@ -138,11 +152,65 @@ def decodeReferences(led_to_entry, max_led_num):
     
     return entry_to_leds if not bad_references else None
 
+def requestSpeeds(csv_reader, direction, speed_type):
+    led_to_entry = {}
+    max_led_num = 0
+    bad_locations = False
+
+    # Retrieve appropriate entries from csv file
+    row_to_entry = {} # allows entry to be used as key in dictionary. See entry["internal_row_num"]
+    for row_num, entry in enumerate(csv_reader):
+        if entry['Direction'] == "North" and direction == Direction.SOUTH:
+            continue
+        if entry['Direction'] == "South" and direction == Direction.NORTH:
+            continue
+        if int(entry['LED Number']) in led_to_entry:
+            log(f"Duplicate LED Number encountered: {int(entry['LED Number'])}")
+            bad_locations = True
+        else:
+            led_to_entry[int(entry['LED Number'])] = entry
+            row_to_entry[row_num] = entry
+            entry["internal_row_num"] = row_num # allows entry to be used as key in dictionary
+        max_led_num = max(max_led_num, int(entry['LED Number']))
+
+    # Search for missing LED numbers
+    for led_num in range(1, max_led_num + 1):
+        if led_num not in led_to_entry:
+            log(f"Missing LED Number: {led_num}")
+            bad_locations = True
+
+    if bad_locations:
+        log("Aborting due to bad location entries in input file.")
+        return False
+
+    # Decode entries that reference other LED numbers
+    entry_to_leds = decodeReferences(led_to_entry, max_led_num)
+    if entry_to_leds == None:
+        log("Aborting due to bad references in input file.")
+        return False                
+
+    # Iterate through LED entries and retrieve data
+    speeds = [-1] * (max_led_num + 1)
+    for entry_row_num, leds in entry_to_leds.items():
+        entry = row_to_entry[entry_row_num]
+        speed = requestData(entry, speed_type)
+        if speed == -1:
+            log(f"No speed data found for LEDs {leds}")
+            continue
+
+        for led_num in leds:
+            speeds[led_num] = speed
+        log(f"Retrieved speed {speed} for LEDs {leds}")
+
+    # Replace negative values with 0
+    speeds = [0 if speed < 0 else speed for speed in speeds]
+    return speeds
+
 # ================================
 # Main Function
 # ================================
 
-def main(direction, key, csv_filename, output_filename, output_filename_2):
+def main(speed_type, direction, key, csv_filename, output_filename, output_filename_2):
     if direction == Direction.UNKNOWN:
         log("Invalid direction provided. Try North or South.")
         return False
@@ -150,68 +218,22 @@ def main(direction, key, csv_filename, output_filename, output_filename_2):
     try:
         with open(csv_filename, 'r') as input_file:
             csv_reader = csv.DictReader(input_file, dialect='excel')
-            led_to_entry = {}
-            max_led_num = 0
-            bad_locations = False
-
-            # Retrieve appropriate entries from csv file
-            row_to_entry = {} # allows entry to be used as key in dictionary. See entry["internal_row_num"]
-            for row_num, entry in enumerate(csv_reader):
-                if entry['Direction'] == "North" and direction == Direction.SOUTH:
-                    continue
-                if entry['Direction'] == "South" and direction == Direction.NORTH:
-                    continue
-                if int(entry['LED Number']) in led_to_entry:
-                    log(f"Duplicate LED Number encountered: {int(entry['LED Number'])}")
-                    bad_locations = True
-                else:
-                    led_to_entry[int(entry['LED Number'])] = entry
-                    row_to_entry[row_num] = entry
-                    entry["internal_row_num"] = row_num # allows entry to be used as key in dictionary
-                max_led_num = max(max_led_num, int(entry['LED Number']))
-
-            # Search for missing LED numbers
-            for led_num in range(1, max_led_num + 1):
-                if led_num not in led_to_entry:
-                    log(f"Missing LED Number: {led_num}")
-                    bad_locations = True
-
-            if bad_locations:
-                log("Aborting due to bad location entries in input file.")
-                return False
-
-            # Decode entries that reference other LED numbers
-            entry_to_leds = decodeReferences(led_to_entry, max_led_num)
-            if entry_to_leds == None:
-                log("Aborting due to bad references in input file.")
-                return False                
-
-            # Iterate through LED entries and retrieve data
-            current_speeds = [-1] * (max_led_num + 1)
-            for entry_row_num, leds in entry_to_leds.items():
-                entry = row_to_entry[entry_row_num]
-                current_speed = requestData(entry)
-                if current_speed == -1:
-                    log(f"No speed data found for LEDs {leds}")
-                    continue
-
-                for led_num in leds:
-                    current_speeds[led_num] = current_speed
-                log(f"Retrieved speed {current_speed} for LEDs {leds}")
-
-            # Replace negative values with 0
-            current_speeds = [0 if speed < 0 else speed for speed in current_speeds]
+            speeds = requestSpeeds(csv_reader, direction, speed_type)
 
             # Debug log to check the contents of current_speeds
-            log(f"Final current_speeds: {current_speeds}")
+            log(f"Final current_speeds: {speeds}")
 
             # Save the results to the output file in binary format
-            byte_array = bytearray(current_speeds)
+            byte_array = bytearray(speeds)
             log(f"Writing byte array of length {len(byte_array)} to {output_filename}")
             with open(output_filename, 'wb') as out_file:
                 out_file.write(byte_array)
-            with open(output_filename_2, 'wb') as out_file:
-                out_file.write(byte_array)
+            
+            # Save the results to second output file if present (for backwards compatability)
+            if output_filename_2 is not None:
+                log(f"Writing byte array of length {len(byte_array)} to {output_filename_2}")
+                with open(output_filename_2, 'wb') as out_file:
+                    out_file.write(byte_array)
 
             log(f"Successfully completed request for {direction.name}")
             return True
@@ -225,8 +247,11 @@ def main(direction, key, csv_filename, output_filename, output_filename_2):
 # ================================
 
 if __name__ == "__main__":
-    # North direction
-    main(Direction.NORTH, API_KEY, INPUT_CSV, OUTPUT_NORTH, OUTPUT_NORTH_2)
-
-    # South direction
-    main(Direction.SOUTH, API_KEY, INPUT_CSV, OUTPUT_SOUTH, OUTPUT_SOUTH_2)
+    if len(sys.argv) > 1 and sys.argv[1] == "typical":
+        log("retrieving typical speeds")
+        main(SpeedType.TYPICAL, Direction.NORTH, API_KEY, INPUT_CSV, OUTPUT_NORTH_TYPICAL, None)
+        main(SpeedType.TYPICAL, Direction.SOUTH, API_KEY, INPUT_CSV, OUTPUT_SOUTH_TYPICAL, None)
+    else:
+        log("retrieving current speeds")
+        main(SpeedType.CURRENT, Direction.NORTH, API_KEY, INPUT_CSV, OUTPUT_NORTH, OUTPUT_NORTH_2)
+        main(SpeedType.CURRENT, Direction.SOUTH, API_KEY, INPUT_CSV, OUTPUT_SOUTH, OUTPUT_SOUTH_2)
